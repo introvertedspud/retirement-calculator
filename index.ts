287e4808db2fcb2ef9bbb2ddcf4912ae834fda9c
Shaun Bonk <7318717+introvertedspud@users.noreply.github.com>
2023-11-17 10:53:13 -0700

    Fix for importing package

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { default as RetirementCalculator } from './src/RetirementCalculator';
+export { CONTRIBUTION_FREQUENCY } from './src/constants/retirementCalculatorConstants';
+export type {
+  ContributionFrequencyType,
+  DetermineContributionType,
+} from './src/types/retirementCalculatorTypes';
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  roots: ['tests'],
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'coverage',
